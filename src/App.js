82f922b79626bd42477bf34a320daa7312661b99
Henrik <henrik.saega@voco.ee>
2024-11-22 11:11:27 +0200

    Erinevad expenses data

--- src/App.js
+++ src/App.js
@@ -2,9 +2,23 @@ import './App.css';
 import ExpenseItem from './components/ExpenseItem.js';
 
 function App() {
+  const expenses = [
+    {
+      date: new Date(2024, 11, 22),
+      title: "New book",
+      price: 19.99
+    },
+    {
+      date: new Date(2024, 11, 22),
+      title: "New Jeans",
+      price: 49.99
+    }
+  ]
+
   return (
     <div className="App">
-      <ExpenseItem/>
+      <ExpenseItem data={expenses[0]}/>
+      <ExpenseItem data={expenses[1]}/>
     </div>
   );
 }
